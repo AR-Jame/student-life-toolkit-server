70df44c5defb0c1fd5a4f55d34bb3cd682f0d06a
AR-Jame <ar.jame333@gmail.com>
2025-09-04 01:23:48 +0600

    User registration, basic error handling, folder structure

---
+++ app/modules/user/user.services.js
@@ -0,0 +1,26 @@
+import { env } from "../../config/env.js";
+import AppError from "../../errorHelper/AppError.js";
+import { User } from "./user.model.js";
+import bcrypt from "bcryptjs";
+
+const createUser = async (payload) => {
+    const { email, password, ...rest } = payload;
+
+    const isUserExist = await User.findOne({ email: email });
+    if (isUserExist) {
+        throw new AppError(500, "Email already exists.")
+    }
+
+    const hashedPassword = await bcrypt.hash(password, parseInt(env.BCRYPT_SALT_ROUND));
+
+    const user = await User.create({
+        email,
+        password: hashedPassword,
+        ...rest
+    });
+    return user
+}
+
+export const userServices = {
+    createUser
+}
